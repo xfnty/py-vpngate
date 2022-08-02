9b0309a54c0b7a8706ba410aa3504fcd0425f38e
ts-vadim <tsentsevitskiy.v@gmail.com>
2022-08-02 17:41:48 +0700

    Refactor

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 rm -r /usr/local/bin/py-vpngate-src &> /dev/null
 rm -r /usr/local/bin/vpngate &> /dev/null
-cp -f -r ./src /usr/local/bin/py-vpngate-src
+cp -f -r ./py-vpngate-src /usr/local/bin/py-vpngate-src
 cp -f ./vpngate /usr/local/bin/vpngate
 chmod +x /usr/local/bin/vpngate
