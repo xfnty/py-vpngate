96e21a322b35c56888431e6200f61706cdbd95ea
Ts. Vadim <tsentsevitskiy.v@gmail.com>
2022-08-16 10:39:49 +0700

    Update __init__.py

--- py-vpngate-src/VPNGate/__init__.py
+++ py-vpngate-src/VPNGate/__init__.py
@@ -1,17 +1,1 @@
-import logging
 
-_console_formatter = logging.Formatter('%(message)s')
-_file_formatter = logging.Formatter('%(levelname)s: %(message)s')
-
-_ch = logging.StreamHandler()
-_ch.setLevel(logging.INFO)
-_ch.setFormatter(_console_formatter)
-
-_fh = logging.FileHandler('./vpngate.log', mode='w')
-_fh.setLevel(logging.NOTSET)
-_fh.setFormatter(_file_formatter)
-
-logging.basicConfig(
-	level=logging.DEBUG,
-	handlers=[_ch, _fh]
-)
