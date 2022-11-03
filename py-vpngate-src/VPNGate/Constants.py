9f7b4dd8783f16875e99d4cad89f55d4be6bd6f2
Ts. Vadim <tsentsevitskiy.v@gmail.com>
2022-11-03 13:10:35 +0700

    Update VPN filtering algorithm (#16)

---
+++ py-vpngate-src/VPNGate/Constants.py
@@ -0,0 +1,8 @@
+
+APP_VPN_PROFILE_NAME = 'VPNGate Profile'
+APP_LOG_FILENAME = 'vpngate.log'
+
+VPNGATE_API_URL = 'https://www.vpngate.net/api/iphone'
+VPNGATE_CACHE_FILENAME = '.vpngate_cache'
+VPNGATE_UNAVAILABLE_FILTER_FILENAME = '.vpngate_filter'
+VPNGATE_TEMP_CACHE_FILENAME = '.vpngate_temp_cache'
